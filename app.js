17eaffd24188394917be894481e08a1a1d79cc27
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-05-31 11:31:11 +0500

    commit1

---
+++ app.js
@@ -0,0 +1,76 @@
+//********************** Chapter : 35-38 ********************** */
+
+
+//********************** Task : 01 ********************** */
+
+// function abc(){
+//     var date = new Date();
+//     document.write(date)
+// }
+
+// abc();
+
+
+//********************** Task : 02 ********************** */
+
+// function name(){
+//     var firstName = prompt("Enter your first name")
+//     var lastName = prompt("Enter your last name")
+
+//     document.write(firstName + " " + lastName)
+// }
+
+// name()
+
+
+//********************** Task : 03 ********************** */
+
+// function sum(){
+//     var firstNum = +prompt("Enter your first name")
+//     var secondNum = +prompt("Enter your last name")
+
+//     document.write(firstNum + secondNum)
+// }
+
+// sum()
+
+
+//********************** Task : 04 ********************** */
+
+// function calc(){
+//     var firstNum = +prompt("Enter your first name")
+//     var secondNum = +prompt("Enter your last name")
+// var operator = prompt("Enter your operator")
+//     if(operator === "+"){
+//         document.write(firstNum+secondNum)
+//     }
+//     else if(operator === "-"){
+//         document.write(firstNum-secondNum)
+//     }
+//     else if(operator === "*"){
+//         document.write(firstNum*secondNum)
+//     }
+//     else if(operator === "/"){
+//         document.write(firstNum/secondNum)
+//     }else{
+//         document.write("enter right operator")
+//     }
+// }
+
+// calc()
+
+
+//********************** Task : 07 ********************** */
+
+// function count(){
+//     var startNum = +prompt("Enter your first name")
+//     var endNum = +prompt("Enter your last name")
+//     for(var i = startNum; i <= endNum; i++){
+//         document.write(i + "<br>")
+//     }
+// }
+// count();
+
+
+
+
