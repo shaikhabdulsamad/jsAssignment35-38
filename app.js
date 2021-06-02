de0dc6547ea36fb88322c6b03e0c25febb4d2bad
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-02 13:28:11 +0500

    commit2

--- app.js
+++ app.js
@@ -37,40 +37,134 @@
 
 //********************** Task : 04 ********************** */
 
-// function calc(){
-//     var firstNum = +prompt("Enter your first name")
-//     var secondNum = +prompt("Enter your last name")
-// var operator = prompt("Enter your operator")
-//     if(operator === "+"){
-//         document.write(firstNum+secondNum)
+// function abc(num1, opr, num2) {
+//     if (opr === "+") {
+//         return num1 + num2
 //     }
-//     else if(operator === "-"){
-//         document.write(firstNum-secondNum)
-//     }
-//     else if(operator === "*"){
-//         document.write(firstNum*secondNum)
-//     }
-//     else if(operator === "/"){
-//         document.write(firstNum/secondNum)
-//     }else{
-//         document.write("enter right operator")
+//     else if (opr === '-') {
+//         return num1 - num2
+//     } else {
+//         return 0
 //     }
 // }
+// var n1 = +prompt("Enter first num1")
+// var n2 = +prompt("Enter first num2")
+// var oprt = prompt("Enter operator")
+// var a = abc(n1, oprt, n2);
 
-// calc()
+
+// alert(a)
+
+
+//********************** Task : 05 ********************** */
+
+// function sumOfSquares(num) {
+
+//     for(var i=0; i <= num; i++){
+//         var sum = i*i;
+// }
+//      document.write("The sum of squares for numbers " +num+ " is " +sum ) ;
+// }
+
+// var num = +prompt("Enter a number:");
+// sumOfSquares(num);
+
+
+//********************** Task : 06 ********************** */
+
 
 
 //********************** Task : 07 ********************** */
 
-// function count(){
-//     var startNum = +prompt("Enter your first name")
-//     var endNum = +prompt("Enter your last name")
-//     for(var i = startNum; i <= endNum; i++){
+// function count(num1,num2){
+  
+//     for(var i = num1; i <= num2; i++){
 //         document.write(i + "<br>")
 //     }
 // }
-// count();
+
+// var startNum = +prompt("Enter start number")
+// var endNum = +prompt("Enter end number")
+
+// count(startNum,endNum);
 
 
 
+//********************** Task : 08 ********************** */
+
+
+// function calcHypo(num1,num2){
+
+//     var hypo = num1 + num2
+
+//     function calcSquare(){
+//        var square = hypo* hypo
+//         document.write("Hypotenuse <sup>2</sup> = " + square )
+//     }
+
+//     document.write("Base = " + num1 + "<br>" +"Perpendicular = "+ num2 + "<br>" )
+//     document.write("Hypotenuse = " + hypo + "<br>" )
+//     calcSquare();
+// }
+// var base = +prompt("Enter value of base")
+// var perpendicular = +prompt("Enter value of perpendicular")
+// calcHypo(base,perpendicular)
+
+
+//********************** Task : 09 ********************** */
+
+// function area(num1,num2){
+// var a = num1*num2;
+// document.write("Area of triangle is : "+a +"cm");
+// }
+// var width = +prompt("Enter width")
+// var height = +prompt("Enter height")
+// area(width,height);
+
+// area(10,20);
+
+
+//********************** Task : 10 ********************** */
+
+// function abc(word){
+//     var check = word.split("").reverse().join("")
+//     if(check === word){
+//         document.write("Its a palindrome word")
+//     }else{document.write("Its not a palindrome word")}
+// }
+
+
+// var a = prompt("Enter your word")
+// abc(a)
+
+
+//********************** Task : 11 ********************** */
+
+
+// function capitalizeWord(words) {
+//     var separateWord = words.toLowerCase().split(' ');
+//     for (var i = 0; i < separateWord.length; i++) {
+//        separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+//        separateWord[i].substring(1);
+//     }
+//     return separateWord.join(' ');
+//  }
+//  var userInput = prompt("Enter any sentence","the quick brown fox")
+// document.write(capitalizeWord(userInput));
+
+
+//********************** Task : 12 ********************** */
+
+// function longestWord(word){
+//     var long = "";
+//     var separateWord = word.split(" ");
+//     for(var i = 0; i < separateWord.length; i++){
+//         if(separateWord[i].length > long.length){
+//             long = separateWord[i]
+//         }
+//     }
+//     return long
+// }
+// var result = longestWord("Web Development Tutorial")
+// console.log(result)
 
